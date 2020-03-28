ed62d3a740806dcd5fc83470327f22a556488829
Nico-toulouse <nicoramon@hotmail.com>
2020-03-28 11:53:00 +0100

    writting minimal function

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,7 +1,22 @@
 const assert = require('assert');
 
+function capitalizeFirstLetters(input) {
+    let originalSentence = input;
+    let word = originalSentence.split('');
+  
+    for (i = 0; i < originalSentence.length; i++) {
+      word[0] = word[0].toUpperCase();
+      if (word[i] === " ") {
+        word[i+1] = word[i+1].toUpperCase();
+      } else {
+        word[i+1] = word[i+1];
+      }
+    }
+    return word.join("");
+}
+
 // Check that capitalizeFirst is a function
-assert.strictEqual(typeof capitalizeFirst, 'function');
+assert.strictEqual(typeof capitalizeFirstLetters, 'function');
 // Check that capitalizeFirst transforms javaScript correctly
 assert.strictEqual(capitalizeFirstLetters('javaScript'), 'JavaScript');
 // Check that it works for a 1-character string
